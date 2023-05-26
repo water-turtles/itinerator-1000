a31578c1ef793eb271ff621a9c94b91143b9d13b
jason-breen <jason.breen@gmail.com>
2023-05-26 15:32:26 -0400

    Update import statements and linting fixes

--- backend/generate.ts
+++ backend/generate.ts
@@ -1,4 +1,4 @@
-import openaiClient from './api';
+import openaiClient from './api.js'
 
 const generate = async (tripDescription: string) => {
   const daVinci = async (tripDescription: string) => {
@@ -6,34 +6,34 @@ const generate = async (tripDescription: string) => {
       model: 'text-davinci-003',
       prompt: `Convert the following natural language description into a SQL query:\n\n${tripDescription}`,
       max_tokens: 100,
-      temperature: 0,
-    });
-    return response.data.choices[0].text;
-  };
+      temperature: 0
+    })
+    return response.data.choices[0].text
+  }
 
   const chatGPT = async (tripDescription: string) => {
     const message = [
-      { role: 'system', content: `You are a translator from plain English to SQL.` },
+      { role: 'system', content: 'You are a translator from plain English to SQL.' },
       {
         role: 'user',
-        content: `Convert the following natural language description into a SQL query:\n\nShow all all the elements in the table users`,
+        content: 'Convert the following natural language description into a SQL query:\n\nShow all all the elements in the table users'
       },
       { role: 'assistant', content: 'SELECT * FROM users;' },
       {
         role: 'user',
-        content: `Convert the following natural language description into a SQL query:\n\n${tripDescription}`,
-      },
-    ];
+        content: `Convert the following natural language description into a SQL query:\n\n${tripDescription}`
+      }
+    ]
     const response = await openaiClient.createChatCompletion({
       model: 'gpt-3.5-turbo',
-      messages: message,
-    });
+      messages: message
+    })
 
-    return response.data.choices[0].message?.content;
-  };
+    return response.data.choices[0].message?.content
+  }
 
-  const sqlQuery = await chatGPT(tripDescription);
-  return sqlQuery;
-};
+  const sqlQuery = await chatGPT(tripDescription)
+  return sqlQuery
+}
 
-export default generate;
+export default generate
--- backend/server.ts
+++ backend/server.ts
@@ -1,13 +1,13 @@
-import express, { Request, Response, NextFunction } from 'express';
-import cookieParser from 'cookie-parser';
-import dotenv from 'dotenv';
-import generate from './generate';
-dotenv.config();
-const PORT = 3000;
+import express, { Request, Response, NextFunction } from 'express'
+import cookieParser from 'cookie-parser'
+import dotenv from 'dotenv'
+import generate from './generate.js'
+dotenv.config()
+const PORT = 3000
 
-const app = express();
-app.use(cookieParser());
-app.use(express.json());
+const app = express()
+app.use(cookieParser())
+app.use(express.json())
 
 // Main page
 // app.get('/', (req, res) => {
@@ -15,15 +15,15 @@ app.use(express.json());
 // });
 
 app.post('/generate', async (req, res) => {
-  const { tripDescription } = req.body;
+  const { tripDescription } = req.body
   try {
-    const query = await generate(tripDescription);
-    res.json({ query });
+    const query = await generate(tripDescription)
+    res.json({ query })
   } catch (error) {
-    console.error(error);
-    res.status(500).send('Error in generate');
+    console.error(error)
+    res.status(500).send('Error in generate')
   }
-});
+})
 
 // 404 redirect to index.html for react router
 // app.use((req, res) =>
@@ -35,13 +35,13 @@ app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   const defaultErr = {
     log: `Express global error handler caught unhandled middleware error: ${err}`,
     status: 500,
-    message: { err: 'An error occurred' },
-  };
-  const errorObj = Object.assign({}, defaultErr, err);
-  console.log(errorObj.log);
-  return res.status(errorObj.status).json(errorObj.message);
-});
+    message: { err: 'An error occurred' }
+  }
+  const errorObj = Object.assign({}, defaultErr, err)
+  console.log(errorObj.log)
+  return res.status(errorObj.status).json(errorObj.message)
+})
 
 app.listen(PORT, () => {
-  console.log('Server listening on Port', PORT);
-});
+  console.log('Server listening on Port', PORT)
+})
