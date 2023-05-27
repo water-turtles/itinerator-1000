4b93f58ff2161060d8c90da5117dc1934a8e0670
imjho <jholokar@wesleyan.edu>
2023-05-27 09:49:07 -0700

    update linting

--- backend/server.ts
+++ backend/server.ts
@@ -1,18 +1,18 @@
-import express, { Request, Response, NextFunction } from 'express';
-import cookieParser from 'cookie-parser';
-import dotenv from 'dotenv';
+import express, { Request, Response, NextFunction } from 'express'
+import cookieParser from 'cookie-parser'
+import dotenv from 'dotenv'
 // import generate from './generate';
-dotenv.config();
-const PORT = 3000;
+dotenv.config()
+const PORT = 3000
 
-const app = express();
-app.use(cookieParser());
-app.use(express.json());
+const app = express()
+app.use(cookieParser())
+app.use(express.json())
 
 // Main page
 app.get('/api', (req, res) => {
-  res.status(200).send('backend');
-});
+  res.status(200).send('backend')
+})
 
 // app.post('/generate', async (req, res) => {
 //   const { tripDescription } = req.body;
@@ -35,13 +35,13 @@ app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   const defaultErr = {
     log: `Express global error handler caught unhandled middleware error: ${err}`,
     status: 500,
-    message: { err: 'An error occurred' },
-  };
-  const errorObj = Object.assign({}, defaultErr, err);
-  console.log(errorObj.log);
-  return res.status(errorObj.status).json(errorObj.message);
-});
+    message: { err: 'An error occurred' }
+  }
+  const errorObj = Object.assign({}, defaultErr, err)
+  console.log(errorObj.log)
+  return res.status(errorObj.status).json(errorObj.message)
+})
 
 app.listen(PORT, () => {
-  console.log('Server listening on Port', PORT);
-});
+  console.log('Server listening on Port', PORT)
+})
