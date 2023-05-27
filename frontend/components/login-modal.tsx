68976b23def4b68bc9e670cf49b6f60a33e92d7e
jason-breen <jason.breen@gmail.com>
2023-05-27 14:57:13 -0400

    Minor progress, starting to bring in DaisyUI components

--- frontend/components/login-modal.tsx
+++ frontend/components/login-modal.tsx
@@ -1,10 +1,25 @@
+import React from 'react'
+
 export default function LoginModal () {
   return (
-      <div id="login-modal">
-        <h1>Let's get you logged in!</h1>
-        <p>Username</p>
-        <p>Password</p>
-        <p>New User? Sign Up Here!</p>
+    <div id="login-modal">
+      <h1>Let's get you logged in!</h1>
+      <div className="form-control w-full max-w-xs">
+        <label className="label">
+          <span className="label-text">Username</span>
+        </label>
+        <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
       </div>
+      <div className="form-control w-full max-w-xs">
+        <label className="label">
+          <span className="label-text">Password</span>
+        </label>
+        <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
+      </div>
+
+      <button className="btn">Submit</button>
+      <p>Forgot password?</p>
+      <p>New User? Sign Up Here!</p>
+    </div>
   )
 }
