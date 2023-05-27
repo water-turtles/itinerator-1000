3666535392d78b11d155d7891439a8c354e1f71b
jason-breen <jason.breen@gmail.com>
2023-05-27 14:51:35 -0400

    Update to ESLint config for react  - Added and installed react plugins  - Updated config file    - Disable 'no unescaped entities' rule

---
+++ .eslintrc.ts
@@ -0,0 +1,27 @@
+export default {
+  ignorePatterns: ['**/dist/*'],
+  extends: ['standard', 'plugin:react/recommended', 'plugin:@typescript-eslint/recommended'],
+  parser: '@typescript-eslint/parser',
+  // parserOptions: {
+  //   ecmaFeatures: {
+  //     jsx: true
+  //   },
+  //   ecmaVersion: 12,
+  //   sourceType: 'module'
+  // },
+  plugins: [
+    'react',
+    '@typescript-eslint'
+  ],
+  root: true,
+  rules: {
+    '@typescript-eslint/no-var-requires': 0,
+    "react/no-unescaped-entities": 0,
+    indent: ['error', 2]
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  }
+}
