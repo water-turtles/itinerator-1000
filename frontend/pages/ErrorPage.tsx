fe172a45a99fbb296f05ab4d1c7cb841992de293
mashkn <mariya.novokhatska@gmail.com>
2023-06-01 14:38:58 -0600

    finished the error page and scaled for mobile

--- frontend/pages/ErrorPage.tsx
+++ frontend/pages/ErrorPage.tsx
@@ -1,20 +1,21 @@
 import React from 'react'
 import { useRouteError } from 'react-router-dom'
 import styles from './ErrorPage.module.css'
+import { AiOutlineArrowRight } from 'react-icons/ai'
 
 const ErrorPage: React.FC = () => {
   const error: any = useRouteError()
   console.error('Error in navigating to desired route: ', error)
 
   return (
-    <div id="error-page" className={styles.container}>
+    <div className={styles.container}>
       <div className={styles.innerText}>
-        <h1>{error.status}</h1>
-        <h2>Uh-oh!</h2>
-        <p>You got lost.</p>
-        <p>
-          <i>{error.statusText || error.message}</i>
-        </p>
+        <p className="text-xl">{error.status}</p>
+        <br/>
+        <p className="text-3xl">Uh-oh!</p>
+        <p className="text-2xl">You took a wrong turn, <br/>an unexpected error occurred.</p>
+        <br/>
+        <p className="text-xl"><a href='/'>Go back home <AiOutlineArrowRight/></a></p>
       </div>
     </div>
   )
